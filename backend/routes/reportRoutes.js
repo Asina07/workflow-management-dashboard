be8169ad9ead96075d58d6475509656947901e68
Asina07 <85829716+Asina07@users.noreply.github.com>
2026-02-23 15:01:18 +0400

    Update reportRoutes.js

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { protect, adminOnly } = require('../middlewares/authMiddleware');
-// const { exportTasksReport, exportUsersReport } = require('../controllers/ReportControllers');
+// const { exportTasksReport, exportUsersReport } = require('../controllers/reportControllers');
 const { exportTasksReport, exportUsersReport } =
   require('../controllers/reportControllers');
 const router = express.Router();
