0d4c98e9f1c59773571fa27de13ff291553d0098
unknown <asinaxabdulla@gmail.com>
2026-02-23 14:27:13 +0400

    minor fix

--- backend/routes/ReportRoutes.js
+++ backend/routes/ReportRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { protect, adminOnly } = require('../middlewares/authMiddleware');
-const { exportTasksReport, exportUsersReport } = require('../controllers/ReportControllers');
-
+// const { exportTasksReport, exportUsersReport } = require('../controllers/ReportControllers');
+const { exportTasksReport, exportUsersReport } =
+  require('../controllers/reportControllers');
 const router = express.Router();
 
 router.get("/export/tasks",protect,adminOnly, exportTasksReport); //export all tasks report as excel/pdf
