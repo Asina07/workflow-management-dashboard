6bc68f602a3241f52be8e9ca21b4a865fafb4696
unknown <asinaxabdulla@gmail.com>
2026-02-27 12:25:06 +0400

    Added loader component

--- frontend/src/pages/Root.jsx
+++ frontend/src/pages/Root.jsx
@@ -2,11 +2,18 @@
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { UserContext } from "../context/userContext";
+import { ClipLoader } from "react-spinners";
 
 const Root = () => {
   const { user, loading } = useContext(UserContext);
 
-  if (loading) return <div className="flex justify-center items-center h-screen">Loading...</div>;
+  {
+    loading && (
+      <div className="flex justify-center items-center h-screen">
+        <ClipLoader size={50} />
+      </div>
+    );
+  }
 
   if (!user) return <Navigate to="/login" replace />;
 
